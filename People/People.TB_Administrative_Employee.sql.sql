933f75259738bee030990aa8421364a26b0e6f40
Jesner <JESNER.MELGARA@ucr.ac.cr>
2023-10-03 23:52:59 -0600

    Update on table People.TB_Employeed

---
+++ People/People.TB_Administrative_Employee.sql.sql
@@ -0,0 +1,9 @@
+--Table People.TB_Administrative_Employee create, People. is SCHEMA From the Data Bases.
+
+CREATE TABLE People.TB_Administrative_Employee(
+Position VARCHAR(200) NOT NULL, --Position of the Job
+Job_Description Varchar(1000) NOT NULL,--Description of the Job
+Salary INT NOT NULL,--Salary of the Employee
+People_ID INT NOT NULL,--ID for Identify it
+FOREIGN KEY (People_ID) REFERENCES People.People(People_ID)
+) 
