449a41b08d45a695a81f52da88d2f9469e4ca428
Jesner <JESNER.MELGARA@ucr.ac.cr>
2023-11-06 23:34:46 -0600

    CRUD Store Procedures about TB_Group_Teacher

--- People/People.TB.Group_Teacher.sql
+++ People/People.TB.Group_Teacher.sql
@@ -1,4 +1,6 @@
 CREATE TABLE People.TB.Group_Teacher(
 	 Id_Teacher VARCHAR(20) NOT NULL,
 	 FOREIGN KEY (Id_Teacher) REFERENCES People.Teacher(Id_Teacher),
+	 Group_ID INT NOT NULL, 
+	 FOREIGN KEY (Group_ID) REFERENCES Group.TB_Group(Group_ID)
 )
