e8b3459fdbf7c0c8db01fe3a44194d0be8492771
Kendall Sánchez <117062888+Kensc19@users.noreply.github.com>
2023-11-18 23:47:50 -0600

    Modified classroom procedures

--- StoredProcedures/Classroom.SP_Insert_Classroom.sql
+++ StoredProcedures/Classroom.SP_Insert_Classroom.sql
@@ -1,20 +1,17 @@
 -- Insert a new Classroom
-CREATE PROCEDURE Classroom.SP_Insert_Classroom
-    @Param_Classroom_ID INT,
-    @Param_Classroom_Number NUMERIC(2, 0),
+ALTER PROCEDURE Classroom.SP_Insert_Classroom
+    @Param_Classroom_Name VARCHAR(50),
     @Param_Building_ID INT
 AS
 BEGIN
     BEGIN TRY
         INSERT INTO Classroom.TB_Classroom (
-            Classroom_ID,
-            Classroom_NUMBER,
+            Classroom_Name,
             BUILDING_ID
             )
         VALUES 
         (
-            @Param_Classroom_ID,
-            @Param_Classroom_Number,
+            @Param_Classroom_Name,
             @Param_Building_ID
         );
     END TRY
@@ -23,3 +20,4 @@ BEGIN
         SELECT ERROR_MESSAGE() AS ERROR;
     END CATCH
 END;
+
