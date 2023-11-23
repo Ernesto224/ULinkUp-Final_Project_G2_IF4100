eebbdb2fc19c1a54bb512e290f4fd715590fa836
Nubia B.V <83447146+nubiabv@users.noreply.github.com>
2023-11-22 18:38:47 -0600

    Update Career.SP_Delete_Career.sql

--- StoredProcedures/Career.SP_Delete_Career.sql
+++ StoredProcedures/Career.SP_Delete_Career.sql
@@ -1,4 +1,7 @@
--- Author: Nubia Brenes Valerín
+-- Author: Nubia Brenes ValerÃ­n
+-- Create date: 10/24/2023
+-- Description: SP to Delete data about Career into the TB_Career table, belonging to the Career schema.
+
 CREATE PROCEDURE Career.SP_Delete_Career
     -- Add the parameters for the stored procedure here
     @Param_Career_ID INT -- Unique numeric identifier for the Career
@@ -6,7 +9,7 @@ AS
 BEGIN
     BEGIN TRY
         IF EXISTS (SELECT TOP 1 1 FROM Career.TB_Career WHERE Career_ID = @Param_Career_ID)
-        -- Validation to check if the building you want to delete exists.
+        -- Validation to check if the Career you want to delete exists.
         BEGIN
             UPDATE Career.TB_Career
             SET
