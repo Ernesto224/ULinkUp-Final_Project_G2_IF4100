7de458ec06bf63f84c91b1f6441cab38f37e01fb
Nubia B.V <83447146+nubiabv@users.noreply.github.com>
2023-11-22 19:37:52 -0600

    Update Career_Subject.SP_Delete_Career_Subject.sql

--- StoredProcedures/Career_Subject.SP_Delete_Career_Subject.sql
+++ StoredProcedures/Career_Subject.SP_Delete_Career_Subject.sql
@@ -1,19 +1,21 @@
--- Author: Nubia Brenes Valerín
-CREATE PROCEDURE Career_Subject.SP_Delete_Career_Subject
+-- Author: Nubia Brenes ValerÃ­n
+-- Create date: 10/24/2023
+-- Description: SP to Delete data about Career into the TB_Career_Subject table, belonging to the Career schema.
+
+CREATE PROCEDURE Career.SP_Delete_Career_Subject
     -- Add the parameters for the stored procedure here
-    @Param_Career_ID    INT,
-	@Param_Subject_ID   INT,
+    @Param_Career_ID INT
 
 AS
 BEGIN
     BEGIN TRY
-        IF EXISTS (SELECT TOP 1 1 FROM Career_Subject.TB_Career_Subject WHERE Career_ID = @Param_Career_ID AND Subject_ID = @Param_Subject_ID)
+        IF EXISTS (SELECT TOP 1 1 FROM Career.TB_Career_Subject WHERE Career_ID = @Param_Career_ID)
         -- Validation to check if the Career_Subject you want to delete exists.
         BEGIN
-            UPDATE Career_Subject.TB_Career_Subject
+            UPDATE Career.TB_Career_Subject
             SET
                 Erased = 0
-            WHERE Career_ID = @Param_Career_ID AND Subject_ID=@Param_Subject_ID;
+            WHERE Career_ID = @Param_Career_ID;
             -- The deletion is done by logical deletion by changing the state of the Erased column from 1 to 0.
         END
         ELSE
