b6fc935f46348310311a1f31b199571f7ee28b66
Kendall Sánchez <117062888+Kensc19@users.noreply.github.com>
2023-11-29 05:36:42 -0600

    Modified SP

--- Stored_Procedures/School.SP_Insert_School.sql
+++ Stored_Procedures/School.SP_Insert_School.sql
@@ -1,9 +1,10 @@
 -- Insert a new school
 --REVISADO
-CREATE OR ALTER PROCEDURE School.SP_Insert_School
+CREATE OR ALTER PROCEDURE [School].[SP_Insert_School]
     @Param_Name_School VARCHAR(50),
 	@Param_School_abbrev VARCHAR(10) NULL,
-    @Param_Description_School VARCHAR(100)
+    @Param_Description_School VARCHAR(100),
+	@Param_Faculty_ID INT
 AS
 BEGIN
     BEGIN TRY
@@ -11,13 +12,15 @@ BEGIN
         (
             School_Name,
 			School_abbrev,
-            School_Description
+            School_Description,
+			Faculty_ID
         )
         VALUES 
         (
 			@Param_Name_School,
 			ISNULL(@Param_School_abbrev, NULL),
-			@Param_Description_School
+			@Param_Description_School,
+			@Param_Faculty_ID
         );
     END TRY
     BEGIN CATCH
